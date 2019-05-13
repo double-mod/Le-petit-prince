5ba3e8e57eefcb4a597df37503f0385dcd862b24
RandomNYW <wenyan801@gmail.com>
2019-05-13 23:22:47 +0900

    1-1和1-3更新

---
+++ 2D Platformer with pic/Assets/Scripts/movePattern.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class movePattern : MonoBehaviour
+{
+    public enum Pattern
+    {
+        upDown,
+        leftRight,
+        breakable
+    }
+
+    public Pattern pattern;
+
+    public float spd;
+
+    public float distance;
+
+    private Rigidbody2D myrigidbody2D;
+
+    private bool shiftFlg = false;
+
+    private Vector2 center;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        center = new Vector2(transform.position.x,transform.position.y);
+        myrigidbody2D = GetComponent<Rigidbody2D>();
+        myrigidbody2D.bodyType = RigidbodyType2D.Static;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(pattern)
+        {
+            case Pattern.upDown:
+                verticalMov();
+                break;
+            case Pattern.leftRight:
+                horizontalMov();
+                break;
+            case Pattern.breakable:
+                durationTest();
+                break;
+        }
+        distanceCheck();
+    }
+
+    private void durationTest()
+    {
+        
+    }
+
+    private void horizontalMov()
+    {
+        Vector2 moveDistance = new Vector2(Time.deltaTime * spd, 0f);
+        transform.position = new Vector2(transform.position.x + moveDistance.x, transform.position.y + moveDistance.y);
+
+        if (Mathf.Abs(transform.position.x - center.x) > distance && distance != 0)
+            shiftFlg = !shiftFlg;
+
+    }
+
+    private void verticalMov()
+    {
+        Vector2 moveDistance = new Vector2(0f, Time.deltaTime * spd);
+        transform.position = new Vector2(transform.position.x + moveDistance.x, transform.position.y + moveDistance.y);
+
+        if (Mathf.Abs(transform.position.y - center.y) > distance&&distance!=0)
+            shiftFlg = !shiftFlg;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (this.GetComponent<Rigidbody2D>().IsTouchingLayers(LayerMask.GetMask("foreground")))
+        {
+            spd = -spd;
+        }
+           
+    }
+
+    private void distanceCheck()
+    {
+        if(shiftFlg)
+        {
+            spd = -spd;
+            shiftFlg = !shiftFlg;
+        }
+    }
+}
---
+++ 2D Platformer with pic/Assets/Shader/Spin.shader
@@ -0,0 +1,58 @@
+﻿Shader "custom/Spin" {
+	Properties{
+			_Color("Color", Color) = (1,1,1,1)
+			_MainTex("Albedo (RGB)", 2D) = "white" {}
+			_RSpeed("RotateSpeed", Range(1, 100)) = 2	//旋转速度
+
+	}
+
+
+		SubShader{
+				//Tags { "RenderType"="Opaque" }
+				//贴图带透明通道，半透明效果设置：
+				Tags{"Queue" = "Transparent" "RenderType" = "Transparent" "IgnoreProjector" = "True"}
+				//Blend选值为：SrcAlpha  和OneMinusSrcAlpha（即1-SrcAlpha）
+				Blend SrcAlpha OneMinusSrcAlpha
+				Pass{
+					Name "Simple"
+					Cull off //双面都显示
+
+					CGPROGRAM
+					#pragma vertex vert
+					#pragma fragment frag
+					#include "UnityCG.cginc"
+
+					sampler2D _MainTex; //变量使用前声明
+					float4 _Color;
+					float _RSpeed;
+
+					struct v2f {
+						float4 pos:POSITION;
+						float4 uv:TEXCOORD0;
+					};
+
+					v2f vert(appdata_base v) {
+						v2f o;
+						o.pos = UnityObjectToClipPos(v.vertex);
+						o.uv = v.texcoord;
+						return o;
+					}
+
+					half4 frag(v2f i) :COLOR{
+
+						float2 uv = i.uv.xy - float2(0.5, 0.5);//UV原点移动到UV中心点
+
+						//float2 rotate = float2(cos(_RSpeed *_Time.x), sin(_RSpeed *_Time.x));
+						//uv = float2(uv.x * rotate.x - uv.y * rotate.y, uv.x * rotate.y + uv.y * rotate.x)
+						//θ旋转角度 UV旋转 (xcosθ - ysinθ,xsinθ+ycosθ)
+						uv = float2(uv.x *cos(-_RSpeed * _Time.x) - uv.y * sin(-_RSpeed * _Time.x),uv.x *sin(-_RSpeed * _Time.x) + uv.y*cos(-_RSpeed * _Time.x));
+						uv += float2(0.5, 0.5);//UV中心转移回原来原点位置
+						half4 c = tex2D(_MainTex, uv) * _Color;
+						return c;
+					}
+
+					ENDCG
+				}
+			}
+				FallBack "Diffuse"
+}
