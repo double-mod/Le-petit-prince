f9e12c8b87cbd9a12ff5f042b282d344322f9c60
double-mod <52765153xuzhixin@gmail.com>
2019-05-15 20:23:51 +0900

    大致修复了移动砖块的BUG

---
+++ 2D Platformer with pic/Assets/Scripts/followBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followBlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        collision.gameObject.transform.parent = transform;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.gameObject.transform.parent = null;
+    }
+}
