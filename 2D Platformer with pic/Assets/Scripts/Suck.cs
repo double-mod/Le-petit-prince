9fd00b5f552097f255ee075b83aa42a1cc121ed8
double-mod <52765153xuzhixin@gmail.com>
2019-05-27 21:20:40 +0900

    加了第三关用的吸走砖块;移动砖块bug的彻底修复？

---
+++ 2D Platformer with pic/Assets/Scripts/Suck.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suck : MonoBehaviour
+{
+    public GameObject[] SuckBlock;
+
+    public Vector3 SuckPoint;
+
+    public float suckFrequncy = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SuckBlock != null)
+        {
+            for (int i = 0; i < SuckBlock.Length; i++)
+            {
+                StartCoroutine(suck(suckFrequncy*i, SuckBlock[i]));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator suck(float time,GameObject block)
+    {
+        yield return new WaitForSeconds(time);
+        block.GetComponent<BlockShake>().StartShake();
+        yield return new WaitForSeconds(2f);
+        block.GetComponent<BlockShake>().EndShake();
+        SuckMove(block);
+    }
+
+    private void SuckMove(GameObject block)
+    {
+        Debug.Log(SuckPoint);
+        Debug.Log(block.transform.localPosition);
+        Vector3 direction = SuckPoint - block.transform.position;
+        Debug.Log(direction);
+        block.GetComponent<Rigidbody2D>().velocity = direction;
+    }
+}
