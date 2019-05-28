7a183cf91b36a9af0193aca770fb998bf5ef6d54
RandomNYW <wenyan801@gmail.com>
2019-05-28 19:25:40 +0900

    更新背景砖和封闭场景等

---
+++ 0528_updated/Assets/Scripts/Dialogue/MessageTriggerLine.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageTriggerLine : MonoBehaviour
+{
+    [SerializeField] MassageTrigger msg;
+    private void Start()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        msg.TriggerMessage();
+    }
+}
