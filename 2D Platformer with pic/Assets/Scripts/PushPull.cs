8e1338a44977b3b07deb4b8a4b59f65e48eaa6e7
double-mod <52765153xuzhixin@gmail.com>
2019-05-31 03:11:55 +0900

    串联完成度百分之90

--- 2D Platformer with pic/Assets/Scripts/PushPull.cs
+++ 2D Platformer with pic/Assets/Scripts/PushPull.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public class PushPull : MonoBehaviour
 {
+    public static bool withBox = false;
+
     [SerializeField]
     private GameObject player;
 
     private Rigidbody2D myRigidbody2D;
 
-    private bool pull=false;    
+    private bool pull=false;
+
+    private bool playerIn = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,15 +33,18 @@ public class PushPull : MonoBehaviour
 
     private void toucchBlock()
     {
-        if (Vector3.Distance(player.transform.position, transform.position) < 2f)
+        if ( Mathf.Abs( player.transform.position.x-transform.position.x) < 1.5f && Mathf.Abs(player.transform.position.y - transform.position.y) < 1.5f)
         {
-            if (Input.GetKeyDown("e") && pull == false)
+            if (Input.GetKeyDown("e") && pull == false&&withBox==false)
             {
                 transform.parent= player.transform;
                 //Debug.Log("按下去了");
                 player.GetComponent<Player>().SetFlipStat(false);
                 myRigidbody2D.isKinematic = false;
+                myRigidbody2D.gravityScale = 1.0f;
                 pull = true;
+                withBox = true;
+                playerIn = true;
             }
             else if (Input.GetKeyDown("e") && pull == true)
             {
@@ -47,6 +54,8 @@ public class PushPull : MonoBehaviour
                 player.GetComponent<Player>().SetFlipStat(true);
                 myRigidbody2D.velocity = new Vector2(0, 0);
                 pull = false;
+                withBox = false;
+                playerIn = false;
             }
             if (player.GetComponent<Player>().getState() =="Air")
             {
@@ -54,12 +63,18 @@ public class PushPull : MonoBehaviour
                 player.GetComponent<Player>().SetFlipStat(true);
                 pull = false;
                 myRigidbody2D.velocity = new Vector2(0, 0);
+                withBox = false;
+                playerIn = false;
             }
         }
         else
         {
-            transform.parent = null;
-            player.GetComponent<Player>().SetFlipStat(true);
+            if(withBox==false||playerIn==true)
+            {
+                transform.parent = null;
+                player.GetComponent<Player>().SetFlipStat(true);
+                playerIn = false;
+            }
             pull = false;
             myRigidbody2D.velocity = new Vector2(0, 0);
         }
